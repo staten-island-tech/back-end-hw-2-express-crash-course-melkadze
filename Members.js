4b1596d348867673fe0394158680786a96c2fde1
melkadze <25963683+melkadze@users.noreply.github.com>
2020-01-13 00:13:24 -0500

    finished

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'Nick Melkadze',
+        email: 'nickm@test.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Mick Nelkadze',
+        email: 'mickn@test.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Nick Nelkadze',
+        email: 'nickn@test.com',
+        status: 'active'
+    },
+]
+
+module.exports = members;
